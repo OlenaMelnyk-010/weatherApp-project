9806a5deb6a52b7407441055abd955a0aa268779
Olena <olena.melnyk010@gmail.com>
2022-09-09 18:00:11 +0300

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
+function formatDate(dateUpdate) {
+  let date = new Date(dateUpdate);
+  let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let day = days[date.getDay()];
+  let data = date.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let currentMonth = months[date.getMonth()];
+
+  return `${day}, ${data} ${currentMonth}` 
+}
+
+function formatTime(timeUpdate) {
+  let time = new Date(timeUpdate);
+  let hours = time.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  } 
+  let minutes = time.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  } 
+
+  return `${hours}:${minutes}`
+}
+
 function displayTemperature(responce) {
   document.querySelector("#temperature").innerHTML = Math.round(responce.data.main.temp);
   document.querySelector("#city").innerHTML = responce.data.name;
@@ -5,7 +43,8 @@ function displayTemperature(responce) {
   document.querySelector("#humidity").innerHTML = responce.data.main.humidity; 
   document.querySelector("#wind").innerHTML = Math.round(responce.data.wind.speed); 
   document.querySelector("#feels-temperature").innerHTML = Math.round(responce.data.main.feels_like);
-
+  document.querySelector("#date").innerHTML = formatDate(responce.data.dt * 1000);
+  document.querySelector("#current-time").innerHTML = formatTime(responce.data.dt * 1000);
 }
 
 let apiKey = "c3717712699a76ea2802ba838ac61fc8";
